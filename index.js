6bfc79d3d8ce657e30e228355b567872ad5a470b
jeremy <jeremyaleman468@gmail.com>
2023-10-31 23:06:06 -0400

    complete

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = function(){
+    let a = 2000
+    let b = 100
+    return a / b
+}
+
+let a = 2000
+let b = 100
+
+let square = (number)=>{
+    return number * number 
+}
+
+let add = (plus , minus)=>{
+    return plus + minus 
+}
